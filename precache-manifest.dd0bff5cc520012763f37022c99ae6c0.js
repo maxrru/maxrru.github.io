1a927138c801a35d7e8436a9040a18c3132e3c18
maximilian.ungar <maximilian.ungar@klarna.com>
2020-06-05 19:35:57 +0200

    update website

---
+++ precache-manifest.dd0bff5cc520012763f37022c99ae6c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b6a12bdfd8f2541d627603961827d1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11470d0c60520b275221",
+    "url": "/static/css/2.b9b2dd63.chunk.css"
+  },
+  {
+    "revision": "c981cc2633e1293114ae",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "11470d0c60520b275221",
+    "url": "/static/js/2.ddfa2abc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ddfa2abc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c981cc2633e1293114ae",
+    "url": "/static/js/main.a1e04969.chunk.js"
+  },
+  {
+    "revision": "3d3f3abf28ace0d84619",
+    "url": "/static/js/runtime-main.3c48109f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fb99d764508b9277882e66c67a95385f.js"
+  "/precache-manifest.dd0bff5cc520012763f37022c99ae6c0.js"
 );
 
 self.addEventListener('message', (event) => {
