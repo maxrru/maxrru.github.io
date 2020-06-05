646cab2aecb0e83cfdda6b61f47466feca72c2cb
maximilian.ungar <maximilian.ungar@klarna.com>
2020-06-05 18:26:24 +0200

    Deploy wishlist for testing

---
+++ precache-manifest.fb99d764508b9277882e66c67a95385f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4c70aa8a27c768ac9744c9762ad801e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d55bf3c6949d421a0af",
+    "url": "/static/css/2.b9b2dd63.chunk.css"
+  },
+  {
+    "revision": "134d53df958d8baf2d5f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5d55bf3c6949d421a0af",
+    "url": "/static/js/2.4e49ad5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4e49ad5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "134d53df958d8baf2d5f",
+    "url": "/static/js/main.a5269bc2.chunk.js"
+  },
+  {
+    "revision": "3d3f3abf28ace0d84619",
+    "url": "/static/js/runtime-main.3c48109f.js"
+  }
+]);
